cc44ef2f84644b4463b018fc0070c4d69c690c05
Raymond <raymond808state1@gmail.com>
2017-06-14 22:18:50 -1000

    Added about page and hbs

--- server.js
+++ server.js
@@ -1,14 +1,25 @@
 const express = require('express');
+const hbs = require('hbs');
 const port = process.env.PORT || 3000;
 
 var app = express();
 
-app.use(express.static(__dirname + '/../../HardWire Innovation/Forme Tech/'));
+//app.use(express.static(__dirname + '/../../HardWire Innovation/Forme Tech/'));
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine', 'hbs');
 
-// app.get('/', (req, res) => {
-//   res.send('<h1>Hello Express!</h1>');
-// });
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', (req, res) => {
+  res.send('<h1>Hello Express!</h1>');
+});
 
 app.listen(port, () => {
   console.log(`Server is up on port ${port}`);
 });
+
+app.get('/about', (req, res) => {
+  res.render('about.hbs',{
+    pageTitle: 'About Page'
+  })
+})
